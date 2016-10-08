d00d23af7358d391f9338b99bddf7a0f1e42b454
kuviman <kuviman@gmail.com>
2016-10-08 14:55:30 +0300

    Labyrinth generation

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from random import *
 
 pygame.init()
 
@@ -9,9 +10,65 @@ screen = pygame.display.set_mode(SCREEN_SIZE, FULLSCREEN)
 font = pygame.font.SysFont(pygame.font.get_default_font(), 32)
 clock = pygame.time.Clock()
 
+size = 16
+dsu = []
+left = []
+down = []
+for i in range(size):
+    dsu.append([])
+    left.append([])
+    down.append([])
+    for j in range(size):
+        dsu[-1].append(i * size + j)
+        left[-1].append(False)
+        down[-1].append(False)
+def merge(x, y):
+    for i in range(size):
+        for j in range(size):
+            if dsu[i][j] == y:
+                dsu[i][j] = x
+
+scale = (SCREEN_SIZE[1] - 100) / size
+
+while True:
+    poss = []
+    for i in range(size):
+        for j in range(size - 1):
+            if dsu[i][j] != dsu[i][j + 1]:
+                poss.append(((i, j), (i, j + 1)))
+    for i in range(size - 1):
+        for j in range(size):
+            if dsu[i][j] != dsu[i + 1][j]:
+                poss.append(((i, j), (i + 1, j)))
+    if len(poss) == 0:
+        break
+    d = choice(poss)
+    merge(dsu[d[0][0]][d[0][1]], dsu[d[1][0]][d[1][1]])
+    if d[0][0] == d[1][0]:
+        left[d[1][0]][d[1][1]] = True
+    else:
+        down[d[1][0]][d[1][1]] = True
+
+def transform(x, y):
+    return (SCREEN_SIZE[0] / 2 + (x - size / 2) * scale,
+            SCREEN_SIZE[1] / 2 + (y - size / 2) * scale)
+
+def line(x1, y1, x2, y2):
+    x1, y1 = transform(x1, y1)
+    x2, y2 = transform(x2, y2)
+    pygame.draw.line(screen, (0, 0, 0), (x1, y1), (x2, y2), 2)
+
+def render():
+    screen.fill((200, 200, 255))
+    for i in range(size + 1):
+        for j in range(size + 1):
+            if ((i == 0 or i == size) and j < size) or (j < size and not down[i][j]):
+                line(i, j, i, j + 1)
+            if ((j == 0 or j == size) and i < size) or (i < size and not left[i][j]):
+                line(i, j, i + 1, j)
+
 running = True
 while running:
-    screen.fill((200, 200, 255))
     for e in pygame.event.get():
         if e.type == QUIT:
             running = False
@@ -19,6 +76,7 @@ while running:
             if e.key == K_ESCAPE:
                 running = False
     dt = clock.tick()
+    render()
     screen.blit(font.render("FPS: " + str(int(clock.get_fps())), True, (0, 0, 0)), (0, 0))
     pygame.display.flip()
 
